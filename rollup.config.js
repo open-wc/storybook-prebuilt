480049d268c4b20db03880fe8a886442269ea59a
Lars den Bakker <larsdenbakker@gmail.com>
2019-12-10 23:43:22 +0100

    fix: export assertIsFn

--- rollup.config.js
+++ rollup.config.js
@@ -54,6 +54,8 @@ export default {
           'getCustomElements',
           'isValidComponent',
           'isValidMetaData',
+          // unknown
+          'assertIsFn',
         ],
       },
     }),
